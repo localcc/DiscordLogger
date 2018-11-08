b9f787552b79785684de4cf41ee56aa14142f1fd
localcChicken <42898191+localcChicken@users.noreply.github.com>
2018-11-08 22:27:51 +0300

    Image caching support, documentation

--- Logger.js
+++ Logger.js
@@ -1,13 +1,48 @@
 const MongoClient = require('mongodb').MongoClient;
 const transformer = require('./ObjectsTransformer.js');
+const request = require('request');
 
 module.exports = class Logger {
     constructor () {
         dsClient.on('messageDelete', this.message_delete);
+        dsClient.on('message', this.message_withAttacment);
         dsClient.on('messageUpdate', this.message_update);
         dsClient.on('voiceStateUpdate', this.voice_state_update);
     }
-
+    message_withAttacment (message) {
+        if(message.type !== 'DEFAULT') return;
+        if(message.system) return;
+        if (!message.guild || !message.channel || !message.author || !message.member) return;
+        if (message.guild.id !== process.env.SERVER_ID) return;
+        if(message.attachments.size > 0) {
+            transformer.message(message).attachments.forEach(attached => {
+                request({
+                    url: 'https://api.imgur.com/3/image',
+                    method: 'POST',
+                    formData: {
+                        'image': `${attached.url}`
+                    },
+                    headers: {
+                        'Authorization': `Client-ID ${process.env.ImgurAPI}`
+                    }
+                }, (err, response, data) => {
+                    let parsedResponse = JSON.parse(response.body);
+                    if(parsedResponse.status == 200){
+                        MongoClient.connect(process.env.MONGODB, function(err, db) {
+                            const collection = db.db(process.env.DB).collection("logs");
+                            collection.insertOne({type: 'imgur_URL', message: transformer.message(message), URL: parsedResponse.data.link,timestamp: Date.now()}, (err) => {if (err) throw err});
+                            db.close();
+                        });
+                    }else{
+                        console.log(`An error occured, response status: ${parsedResponse.status}`);
+                    }
+                    if(err !== null){
+                        console.log(`An unexpected error occured, err: ${err}`);
+                    }
+                });
+            })
+        }
+    }
     message_delete (message) {
         if (message.type !== 'DEFAULT') return;
         if (message.system) return;
